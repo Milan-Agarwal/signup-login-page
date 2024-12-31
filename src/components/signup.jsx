8b32cad2518109bafe4491b98a3313be4ebbb623
Milan-Agarwal <152780852+Milan-Agarwal@users.noreply.github.com>
2024-12-31 22:02:24 +0530

    Update signup.jsx

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,6 +1,6 @@
 import './signup.css'
 import { useForm } from "react-hook-form"
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import { useNavigate } from 'react-router-dom'
 
 function Signup() {
